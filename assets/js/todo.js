27226b42ac9693d289da3cabf70e91b6b9a8025b
siddhulal <siddhulal01@gmail.com>
2017-08-08 13:11:07 -0400

    functional logic

--- assets/js/todo.js
+++ assets/js/todo.js
@@ -1,4 +1,20 @@
-$("li").click(function(){
+$("ul").on("click", "li", function(){
     
 	$(this).toggleClass("clicked1");
+});
+
+$("ul").on("click", "span",function(event){
+	$(this).parent().fadeOut(500,function(){
+		$(this).remove();
+	})
+	
+	event.stopPropagation();
+
+});
+
+$("input[type='text']").keypress(function(event){
+    if(event.which===13){
+    	var text=$(this).val();
+    	$("ul").append("<li><span>X</span> " +text+ "</li");
+    }
 });
