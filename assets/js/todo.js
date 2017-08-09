e5460fb45c9c48c3e8b62c2a3da14775be551747
siddhulal <siddhulal01@gmail.com>
2017-08-08 20:24:21 -0400

    project finished

--- assets/js/todo.js
+++ assets/js/todo.js
@@ -15,6 +15,14 @@ $("ul").on("click", "span",function(event){
 $("input[type='text']").keypress(function(event){
     if(event.which===13){
     	var text=$(this).val();
-    	$("ul").append("<li><span>X</span> " +text+ "</li");
+    	$(this).val("");
+
+    	$("ul").append("<li><span><i class='fa fa-trash'></i></span> " +text+ "</li");
+
     }
+});
+
+$(".fa-plus").click(function () {
+	
+	$("input[type='text']").fadeToggle();
 });
